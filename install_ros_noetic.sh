2b8b25ac9401232b6ece3d1a0c0fd9c5e65b9f0f
Marynel Vázquez <marynel.vazquez@yale.edu>
2023-09-14 11:02:47 -0400

    Update install_ros_noetic.sh

--- install_ros_noetic.sh
+++ install_ros_noetic.sh
@@ -14,6 +14,7 @@ sudo apt install ros-noetic-cv-bridge python3-cv-bridge
 sudo apt install python3-vcstool
 sudo apt install python-is-python3
 sudo apt install python3-tk 
+ros-noetic-pr2-description
 
 # setup your environment so that you can find ros commands
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
