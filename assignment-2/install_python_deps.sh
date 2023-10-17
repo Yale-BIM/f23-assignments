f113c27c53399b9a29c99f25f9734030a539e91f
Marynel Vázquez <marynel.vazquez@yale.edu>
2023-10-17 15:54:27 -0400

    Update install_python_deps.sh

--- assignment-2/install_python_deps.sh
+++ assignment-2/install_python_deps.sh
@@ -2,7 +2,7 @@
 pip3 install --user -U pip
 
 # install tensorflow and other python deps
-pip3 install -U --user tensorflow==2.10.0 tensorboard==2.10.1 scipy==1.9.2 matplotlib==3.6.1 scikit-learn==1.1.2 gdown==4.5.1 testresources==2.0.1
+pip3 install -U --user tensorflow==2.13.1 tensorboard==2.14.0 scipy==1.10.1 matplotlib==3.7.3 scikit-learn==1.3.1 gdown==4.7.1 testresources==2.0.1
 
 # install urdf parser for imitation learning 
 pip3 install --user -U urdf-parser-py==0.0.4
